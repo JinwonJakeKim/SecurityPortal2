e8d9f2dee2b8f21596b9f0a8b470e541c49dde4e
Jinwon Kim <Helpdesk@P50405-02.sktelink.com>
2021-07-08 23:26:47 +0900

    Django Book Study Until Chapter 2 Project

---
+++ mysite/pybo/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question
+from django.utils import timezone
+from .forms import QuestionForm, AnswerForm
+
+def index(request):
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list' : question_list}
+
+    return  render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id) #id에 해당하는 Quesiton 객체를 get
+    context = {'question': question} #위에서 get한 question 객체를 text 화
+    return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    """
+    pybo 답변 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+def question_create(request):
+    """
+    pybo  질문 등록
+    """
+    if request.method == 'POST': #저장하기
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm() #request.method 가 'GET'인 경우 호출됨, 질문등록하기
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
